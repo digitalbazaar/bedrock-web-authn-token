94eb3165b989eae288f22a7a75fd370a0892b9fe
Dave Longley <dlongley@digitalbazaar.com>
2018-05-03 10:57:24 -0400

    Add core files.

---
+++ index.js
@@ -0,0 +1,7 @@
+/*!
+ * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+export {TokenService} from './TokenService.js';
+export {LoginController} from './LoginController.js';
