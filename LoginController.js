ff58055093d27785f7aa10f32fb323586cfbcd5a
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-21 13:53:28 -0400

    Update copyright notices.

--- LoginController.js
+++ LoginController.js
@@ -1,7 +1,6 @@
 /*!
  * Copyright (c) 2018-2020 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
 
 import {TokenService} from './TokenService.js';
 
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /*!
- * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2018-2020 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
 
 export {TokenService} from './TokenService.js';
 export {LoginController} from './LoginController.js';
