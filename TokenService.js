af4c8c677b125458216a7e86642d62adcfd43ac0
Dave Longley <dlongley@digitalbazaar.com>
2022-03-12 15:53:44 -0500

    Update test deps.

--- TokenService.js
+++ TokenService.js
@@ -1,7 +1,6 @@
 /*!
- * Copyright (c) 2018-2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
  */
-
 import bcrypt from 'bcryptjs';
 import {httpClient} from '@digitalbazaar/http-client';
 
